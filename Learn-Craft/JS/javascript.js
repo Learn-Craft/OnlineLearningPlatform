8067f7d0967634121e16d17bbf22e013981f22a7
Hasti Donga <hastidonga25@gmail.com>
2024-03-06 20:10:33 -0500

    new navbar

--- Learn-Craft/JS/javascript.js
+++ Learn-Craft/JS/javascript.js
@@ -1,36 +1,57 @@
-$(document).ready(function(){
-    $("#head").load("meta.html");
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-});
+// // slideshow script
 
-// slideshow script
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+  navToggle.addEventListener('click',() =>{
+    navMenu.classList.add('show-menu')
+  })
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+if(navClose){
+  navClose.addEventListener('click',() =>{
+    navMenu.classList.remove('show-menu')
+  })
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
 }
+navLink.forEach(n => n.addEventListener('click', linkAction))
