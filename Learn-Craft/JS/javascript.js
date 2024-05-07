5f827a9e1b1638f96528c8cf9e55074b088d8647
Hasti Donga <hastidonga25@gmail.com>
2024-05-07 10:53:57 -0400

    Updated Home Page

--- Learn-Craft/JS/javascript.js
+++ Learn-Craft/JS/javascript.js
@@ -36,3 +36,31 @@ if(window.innerWidth < 1200){
 }
 
 }
+
+let toggleBtn = document.getElementById('toggle-btn');
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+    toggleBtn.classList.replace('fa-sun','fa-moon');
+    body.classList.add('dark');
+    localStorage.setItem('dark-mode', 'enabled')
+}
+
+const disableDarkMode = () =>{
+    toggleBtn.classList.replace('fa-moon','fa-sun');
+    body.classList.remove('dark');
+    localStorage.setItem('dark-mode', 'disabled')
+}
+
+if(darkMode === 'enabled'){
+    enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+     let darkMode = localStorage.getItem('dark-mode');
+    if(darkMode === 'disabled'){
+        enableDarkMode();
+    }else{
+        disableDarkMode();
+    }
+}
