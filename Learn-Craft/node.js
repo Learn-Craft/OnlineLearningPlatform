b55b1ab2cb3be919e7b301bb0c7222aeff0662a6
Dharav0126 <dharav2626@gmail.com>
2024-06-25 03:38:57 -0400

    Organized way

--- Learn-Craft/node.js
+++ Learn-Craft/node.js
@@ -1,72 +1,31 @@
-// importing the required modules
-const fs = require("fs");
-const http = require("http");
-const express = require ("express");
-// hi
-// reading files
-const home = fs.readFileSync("./Home.html");
-const about = fs.readFileSync("./About.html");
-const contact = fs.readFileSync("./Contact.html");
-const courses = fs.readFileSync("./Courses.html");
-const logIn = fs.readFileSync("./Login.html");
-const playlist = fs.readFileSync("./Playlist.html");
-const profile = fs.readFileSync("./Profile.html");
-const register = fs.readFileSync("./Register.html");
-const teacher_profile = fs.readFileSync("./Teacher_profile.html");
-const teachers = fs.readFileSync("./Teachers.html");
-const update = fs.readFileSync("./Update.html");
-const watch_video = fs.readFileSync("./watch_video.html");
-
-// starting the express app
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static("CSS"))
 
-// creating the server
-const createServer = http.createServer( (request, response) =>{
-    // setting the route
-    let url = request.url;
-    if(url==="/"||url.toLocaleLowerCase()==="/home"){
-        response.end(home);
-    }
-    else if(url.toLocaleLowerCase()==="/about"){
-        response.end(about);
-    }
-    else if(url.toLocaleLowerCase()==="/contact"){
-        response.end(contact);
-    }
-    else if(url.toLocaleLowerCase()==="/courses"||url.toLocaleLowerCase()==="/course"){
-        response.end(courses);
-    }
-    else if(url.toLocaleLowerCase()==="/login"){
-        response.end(logIn);
-    }
-    else if(url.toLocaleLowerCase()==="/playlist"){
-        response.end(playlist);
-    }
-    else if(url.toLocaleLowerCase()==="/profile"){
-        response.end(profile);
-    }
-    else if(url.toLocaleLowerCase()==="/register"){
-        response.end(register);
-    }
-    else if(url.toLocaleLowerCase()==="/teacherprofile"){
-        response.end(teacher_profile);
-    }
-    else if(url.toLocaleLowerCase()==="/teachers"||url.toLocaleLowerCase()==="/teacher"){
-        response.end(teachers);
-    }
-    else if(url.toLocaleLowerCase()==="/update"){
-        response.end(update);
-    }
-    else if(url.toLocaleLowerCase()==="/watchvideo"){
-        response.end(watch_video);
-    }
-    else{
-        response.end("Error 404: Page not found");
-    }
+// Serve static files from the CSS directory
+app.use('/CSS', express.static(path.join(__dirname, 'CSS')));
+
+// Serve HTML files using Express
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+app.get('/courses', (req, res) => res.sendFile(path.join(__dirname, 'views', 'courses.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/playlist', (req, res) => res.sendFile(path.join(__dirname, 'views', 'playlist.html')));
+app.get('/profile', (req, res) => res.sendFile(path.join(__dirname, 'views', 'profile.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/teacherprofile', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teacher_profile.html')));
+app.get('/teachers', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teachers.html')));
+app.get('/update', (req, res) => res.sendFile(path.join(__dirname, 'views', 'update.html')));
+app.get('/watchvideo', (req, res) => res.sendFile(path.join(__dirname, 'views', 'watch_video.html')));
+
+// Handle 404
+app.use((req, res) => {
+    res.status(404).send('Error 404: Page not found');
 });
 
-// Starting the server
-createServer.listen(2000, '127.0.0.1', ()=>{
-    console.log("hello");
-})
+// Start the server
+app.listen(2000, () => {
+    console.log('Server running on http://127.0.0.1:2000');
+});
