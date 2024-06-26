20a14afea0da3aceecced558867e6f78adb6efed
Dharav0126 <dharav2626@gmail.com>
2024-06-26 17:22:48 -0400

    link updated

--- Learn-Craft/node.js
+++ Learn-Craft/node.js
@@ -7,18 +7,18 @@ app.use('/CSS', express.static(path.join(__dirname, 'CSS')));
 
 // Serve HTML files using Express
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
-app.get('/courses', (req, res) => res.sendFile(path.join(__dirname, 'views', 'courses.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/playlist', (req, res) => res.sendFile(path.join(__dirname, 'views', 'playlist.html')));
-app.get('/profile', (req, res) => res.sendFile(path.join(__dirname, 'views', 'profile.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
-app.get('/teacherprofile', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teacher_profile.html')));
-app.get('/teachers', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teachers.html')));
-app.get('/update', (req, res) => res.sendFile(path.join(__dirname, 'views', 'update.html')));
-app.get('/watchvideo', (req, res) => res.sendFile(path.join(__dirname, 'views', 'watch_video.html')));
+app.get('/home.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/about.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
+app.get('/contact.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+app.get('/courses.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'courses.html')));
+app.get('/login.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/playlist.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'playlist.html')));
+app.get('/profile.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'profile.html')));
+app.get('/register.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/teacherprofile.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teacher_profile.html')));
+app.get('/teachers.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'teachers.html')));
+app.get('/update.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'update.html')));
+app.get('/watchvideo.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'watch_video.html')));
 
 // Handle 404
 app.use((req, res) => {
