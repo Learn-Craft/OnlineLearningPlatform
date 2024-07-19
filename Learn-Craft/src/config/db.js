54a2cd6ce113a50ab4344b9d0b917bf427e8d048
Dharav0126 <dharav2626@gmail.com>
2024-07-19 14:17:34 -0400

    db.js updated

--- Learn-Craft/src/config/db.js
+++ Learn-Craft/src/config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
