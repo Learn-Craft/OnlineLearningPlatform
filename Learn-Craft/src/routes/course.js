4d0eee97fed59d592a2e5fa27d522d048cb6555d
Dharav0126 <dharav2626@gmail.com>
2025-01-21 10:10:40 +0530

    jsonwebtoken removed

--- Learn-Craft/src/routes/course.js
+++ Learn-Craft/src/routes/course.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { createCourse, enrollCourse, getCourses } = require('../controllers/courseController');
-const authMiddleware = require('./middleware/authMiddleware');
 
 router.post('/create', authMiddleware, createCourse);
 router.post('/enroll', authMiddleware, enrollCourse);
